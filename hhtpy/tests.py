9f21f218c33ff17fb69376a8db95e195e06b01df
Lars Havstad <larshand92@gmail.com>
2024-09-16 09:51:17 +0200

    Update tests.py

--- hhtpy/tests.py
+++ hhtpy/tests.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from hhtpy import EmpiricalModeDecomposition
+from hhtpy.emd import decompose
 from hhtpy.plot import plot_imfs
 import matplotlib.pyplot as plt
 
@@ -15,31 +15,37 @@ class TestEMDAndPlotting(unittest.TestCase):
         self.t = self.n / self.f_s  # sec
 
         self.y = (
-                0.3 * np.cos(2 * np.pi * 5 * self.t ** 2) +
-                2 * np.cos(2 * np.pi * 1 * self.t) +
-                1 * self.t
+            0.3 * np.cos(2 * np.pi * 5 * self.t**2)
+            + 2 * np.cos(2 * np.pi * 1 * self.t)
+            + 1 * self.t
         )
 
     def test_emd_decomposition(self):
-        emd = EmpiricalModeDecomposition(self.y)
-        emd.decompose()
 
-        self.assertIsNotNone(emd.imfs)
-        self.assertGreater(len(emd.imfs), 0, "IMFs should not be empty after decomposition")
+        imfs, residue = decompose(self.y)
 
-        self.assertIsNotNone(emd.residue, "Residue should not be None after decomposition")
-        self.assertEqual(len(emd.residue), len(self.y), "Residue length should match the input signal length")
+        self.assertIsNotNone(imfs)
+        self.assertGreater(len(imfs), 0, "IMFs should not be empty after decomposition")
+
+        self.assertIsNotNone(residue, "Residue should not be None after decomposition")
+        self.assertEqual(
+            len(residue),
+            len(self.y),
+            "Residue length should match the input signal length",
+        )
 
     def test_plot_imfs(self):
-        emd = EmpiricalModeDecomposition(self.y)
-        emd.decompose()
+        imfs, residue = decompose(self.y)
 
-        fig, axs = plot_imfs(emd.imfs, self.y, emd.residue, x_axis=self.t, show_plot=False)
+        fig, axs = plot_imfs(imfs, self.y, residue, x_axis=self.t, show_plot=False)
 
-        self.assertIsInstance(fig, plt.Figure, "The output should be a matplotlib Figure object")
-        self.assertIsInstance(axs, np.ndarray, "The output should be an array of matplotlib Axes")
+        self.assertIsInstance(
+            fig, plt.Figure, "The output should be a matplotlib Figure object"
+        )
+        self.assertIsInstance(
+            axs, np.ndarray, "The output should be an array of matplotlib Axes"
+        )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
